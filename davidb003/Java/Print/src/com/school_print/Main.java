33e1e920bfca7c82f8626dbb6ea3f0fcb77b1972
Davide <inbox@davideb.net>
2020-12-02 11:38:16 +0100

    First (and last) commit: Print
    Che programma

---
+++ davidb003/Java/Print/src/com/school_print/Main.java
@@ -0,0 +1,13 @@
+package com.school_print;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nome = "Davide";
+        String cognome = "B.";
+
+        System.out.println("+++++++++++++++++++++++\n" +
+                "|      " + nome + " " + cognome + "      |\n" +
+                "+++++++++++++++++++++++");
+    }
+}
